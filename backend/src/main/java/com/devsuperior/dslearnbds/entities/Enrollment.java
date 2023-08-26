9b6877318488132fe5057107cc198a02177acc02
vicenterj1 <vicenterj1@gmail.com>
2023-08-26 17:36:53 -0300

    04-14 Lesson seed, correção - lessonsDone - 2

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Enrollment.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Enrollment.java
@@ -2,10 +2,13 @@ package com.devsuperior.dslearnbds.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 import com.devsuperior.dslearnbds.entities.pk.EnrollmentPK;
@@ -27,6 +30,9 @@ public class Enrollment implements Serializable {
 	// se fosse para tres valores ( nulo, falso ou verdadeiro) usuaria o wrapper ( b maiúsculo).
 	private boolean available;
 	private boolean onlyUpdate;
+	
+	@ManyToMany(mappedBy = "enrollmentsDone")
+	private Set<Lesson> lessonsDone = new HashSet<>();
 	
 	public Enrollment()	{
 	}
--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -29,4 +29,18 @@ INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prer
 
 INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (1, 1, TIMESTAMP WITH TIME ZONE '2022-11-20T13:00:00Z', null, TRUE, FALSE)
 INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (2, 1, TIMESTAMP WITH TIME ZONE '2022-11-20T13:01:00Z', null, TRUE, FALSE)
- 
+
+INSERT INTO tb_lesson  (title, position, section_id) VALUES ( 'Aula 1 do capitulo 1', 1, 1)
+INSERT INTO tb_content (id, text_Content, video_Uri) VALUES ( 1, 'Material de apoio: abc',  'https://www.youtube.com/watch?v=JCjvVsMe-q8')
+
+INSERT INTO tb_lesson  (title, position, section_id) VALUES ( 'Aula 2 do capitulo 1', 2, 1)
+INSERT INTO tb_content (id, text_Content, video_Uri) VALUES ( 2, '',  'https://www.youtube.com/watch?v=JCjvVsMe-q8')
+
+INSERT INTO tb_lesson  (title, position, section_id) VALUES ( 'Aula 3 do capitulo 1', 3, 1)
+INSERT INTO tb_content (id, text_Content, video_Uri) VALUES ( 3, '',  'https://www.youtube.com/watch?v=JCjvVsMe-q8')
+
+INSERT INTO tb_lesson  (title, position, section_id) VALUES ( 'Tarefa do capitulo 1', 4, 1)
+INSERT INTO tb_task    (id, description, question_Count, approval_Count, weight, due_Date) VALUES (1, 'Fazer um trabalho legal', 5, 4, 1.0, TIMESTAMP WITH TIME ZONE '2022-11-25T13:01:00Z') 
+
+INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id) VALUES (1, 1, 1)
+INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id) VALUES (2, 1, 1)
