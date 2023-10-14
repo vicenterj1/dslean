f0e3e9fb0a2daeb38f2eae09051c71498689561d
vicenterj1 <vicenterj1@gmail.com>
2023-10-14 16:39:08 -0300

    04-26 Pré-autorizando métodos por perfil de usuário

--- backend/src/main/java/com/devsuperior/dslearnbds/repositories/DeliverRepository.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/repositories/DeliverRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dslearnbds.entities.Deliver;
 
-public interface DeliveryRepository extends JpaRepository<Deliver, Long> {
+public interface DeliverRepository extends JpaRepository<Deliver, Long> {
 
 }
